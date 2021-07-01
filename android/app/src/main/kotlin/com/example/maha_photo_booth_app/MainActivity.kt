7496dd72cb5a5fe0e0739a196b93aee223b0cd87
Miguel García Reimers <mgarciareimers@gmail.com>
2021-07-01 18:46:13 +0200

    Start of project.

---
+++ android/app/src/main/kotlin/com/example/maha_photo_booth_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.maha_photo_booth_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/commons/utils/app_localizations.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class AppLocalizations {
+  final Locale locale;
+
+  AppLocalizations(this.locale);
+
+  // Helper method that keeps the code in the widgets concise.
+  static AppLocalizations? of(BuildContext context) {
+    return Localizations.of<AppLocalizations>(context, AppLocalizations);
+  }
+
+  // Static member to have a simple access to the delegate from the MaterialApp.
+  static const LocalizationsDelegate<AppLocalizations> delegate = _AppLocalizationsDelegate();
+
+  late Map<String, String> _localizedStrings;
+
+  Future<bool> load() async {
+    String jsonString = await rootBundle.loadString('lang/${locale.languageCode}.json');
+
+    Map<String, dynamic> jsonMap = json.decode(jsonString);
+
+    this._localizedStrings = jsonMap.map((key, value) {
+      return MapEntry(key, value.toString());
+    });
+
+    return true;
+  }
+
+  // Method that will be called from every widget which needs a localized text.
+  String? translate(String key) {
+    return this._localizedStrings[key];
+  }
+}
+
+// LocalizationsDelegate is a factory for a set of localized resources.
+// In this case, the localized strings will be gotten in an AppLocalizations object.
+class _AppLocalizationsDelegate extends LocalizationsDelegate<AppLocalizations> {
+
+  // This delegate instance will never change.
+  const _AppLocalizationsDelegate();
+
+  @override
+  bool isSupported(Locale locale) {
+    return ['en', 'es'].contains(locale.languageCode); // Include all supported language codes.
+  }
+
+  @override
+  Future<AppLocalizations> load(Locale locale) async {
+    AppLocalizations localizations = new AppLocalizations(locale);
+    await localizations.load();
+
+    return localizations;
+  }
+
+  @override
+  bool shouldReload(LocalizationsDelegate<AppLocalizations> old) => false;
+}
